c23fdef1d46eca9aa69fa019346c4d7cbbcde939
doseung <didehtmd@gmail.com>
2024-10-14 01:11:24 +0900

    Sidebar, post_page global_IconButtn 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,26 @@
 import Link from 'next/link'
-import { FC } from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { Dispatch, FC, SetStateAction } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 import { BsRobot } from 'react-icons/bs'
-const Header: FC = () => {
+import IconButton from './IconButton'
+
+type HeaderProps = {
+  isSidebarOpen: boolean
+  setIsSidebarOpen: Dispatch<SetStateAction<boolean>>
+}
+const Header: FC<HeaderProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
   return (
     <header className="flex h-16 items-center justify-between border-b px-4 lg:h-20 lg:px-10">
-      <button className="p-2">
-        <AiOutlineMenu className="h-5 w-5 lg:h-6 lg:w-6" />
-      </button>
+      <IconButton
+        onClick={() => setIsSidebarOpen((t) => !t)}
+        Icon={isSidebarOpen ? AiOutlineClose : AiOutlineMenu}
+      />
       <Link href="/">
         <h1 className="text-3xl font-medium text-slate-600 lg:text-4xl">
           BLOG
         </h1>
       </Link>
-      <Link href="/posts" className="p-2">
-        <BsRobot className="h-6 w-6" />
-      </Link>
+      <IconButton Icon={BsRobot} component={Link} href="/search" />
     </header>
   )
 }
---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function _404() {
+  return (
+    <div className="flex flex-col items-center p-24">
+      <h2>찾을 수 없는 페이지입니다.🚗</h2>
+    </div>
+  )
+}
